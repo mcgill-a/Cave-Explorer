9a6314eb52b2a6a958903064802b47fcbea4b549
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2018-10-23 19:31:16 +0100

    Initial commit

---
+++ pathfinder/pathfinder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pathfinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App Loaded");
+            if (args.Length < 1)
+            {
+                Console.WriteLine("No arguments added");
+            }
+            else
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine(i + " " + args[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
